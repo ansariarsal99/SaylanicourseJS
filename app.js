8b7eef847e82fe4a3188a17a547cbe04b7ee0827
MuhammadArsalan <ansariarsal99@gmail.com>
2020-06-21 15:31:20 +0500

    Js chap 1 to 38 Assingment

---
+++ app.js
@@ -0,0 +1,179 @@
+//  Assingment # 1
+// Chapter # 1
+// Task # 1
+
+//alert("Hello User");
+
+// Task # 2
+
+//alert("Error! Please enter a valid Password");
+
+// Task # 3
+
+//alert("Welcome to JS land... \n Happy coading");
+
+// Task # 4
+
+//alert("Welcome to JS land... ");
+//alert("Happy coading");
+
+
+// Task # 5
+
+console.log("Hello...!I can run javascript through my web browser console");
+
+//Task 6 & 7 in index file
+
+
+// Chapter # 2
+// Task # 1
+
+var username;
+
+// Task # 2
+
+var myName;
+var first= "Muhammad";
+var last = "arsalan";
+var fullName = first + last; 
+myName = fullName;
+
+// Task # 3
+
+var message;
+message = "hello World";
+//alert(message);
+
+// Task # 4
+
+var name = "john Doe";
+var age = "15 year old";
+var course = "Certified Mobile Application Developer";
+//alert(name);
+//alert(age);
+//alert(course);
+
+//Task # 5
+
+/* var text = "PIZZA";
+var x = "";
+for (x of txt) {
+    document.write(x + "<br >");
+  }
+  alert(x);
+ */
+
+   //Task 6 
+   var email = "Example@gmail.com";
+   //alert("My email address is " + email);
+
+  //Task 7
+    
+   var book = "“A smarter way to learn JavaScript”. ";
+  // alert("I am Trying to learn from the Book " + book);
+
+  //Task 8
+
+  //document.write("Yeah..! I can write HTML content through JavaScript <br>");
+
+  //Task 9
+  
+  var sing ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬“";
+  //alert(sing);
+
+
+
+  
+// Chapter # 3
+// Task # 1
+
+var age = 15;
+//alert("I am " + age + " years old");
+
+
+// Task # 2
+  var visit =0;
+  if (visit === null)
+  {
+        visit=0
+  }   
+  visit++
+  
+  //alert("You visit our website " + visit + " times");
+
+  //Task # 3
+  myBirthYear = 1991;
+ // document.write("My Birth year is "+ myBirthYear + "<br> Date type of my Declared no is number" );
+
+  //Task # 4
+   var website = "xyzcloatingstore.com";
+   /* var visitorName = prompt("Please enter your name:");
+   var productType = prompt("Please enter the product you want:");
+   var quantity = prompt("Please enter your Desired quantity:"); */
+   var visitorName = "John Doe";
+   var productType = "T-Shirts";
+   var quantity = 5;
+   document.write(visitorName + " ordered " +quantity +" "+ productType + " on "+ website);
+   
+
+    
+// Chapter # 4
+// Task # 1
+  // var person = "John Doe", carName = "toyta", price = $20000;
+   
+
+   // Task # 2
+   // Legal Variable
+   var abc;
+   var _xyz;
+   var asd123;
+   var abc_xyz;
+   var $kil_1;
+
+   //Illegal Variable
+//   var 1abc;
+//   var @abc;
+//   var 2_asd;
+//   var !addEventListener;
+//   var visit#1;
+
+  //Task # 3
+
+    document.writeln("<h2> A heading stating Rules for naming JS variables </h2><br><br>");
+    document.write("Variable names can only contain number, $, and _ . For example $my_1stVariable<br>");
+    document.write("Variables must begin with a letter, $ or _. For example $name, _name or name <br>");
+    document.write("Variable names are case sensitive <br>"); 
+    document.write("Variable names should not be JS keywords<br>"); 
+  
+  
+    // Chapter # 5
+    // Task # 1
+    var a =5;
+    var b = 3;
+    var add = a + b;
+    document.write("The sum of "+ a + " & " + b + " is " + add + "<br>");
+
+        // Task # 2
+        //Subtract
+        var a =5;
+        var b = 3;
+        var sub = a - b;
+        document.write("The subtract of "+ a + " & " + b + " is " + sub + "<br>");
+    
+                //Multiply
+                var a =5;
+                var b = 3;
+                var mul = a * b;
+                document.write("The multiply of "+ a + " & " + b + " is " + mul + "<br>");
+
+                //Divison
+                var a =5;
+                var b = 3;
+                var div = a/b;
+                document.write("The divison of "+ a + " & " + b + " is " + div + "<br>");
+
+            //Modulus
+            var a =5;
+            var b = 3;
+            var mod = a%b;
+            document.write("The modulus of "+ a + " & " + b + " is " + mod + "<br>");
